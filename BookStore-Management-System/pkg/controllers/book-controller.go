6dd283d267246c25f38779f018ce70c5a4f6ebc5
Kush Vardhan <kushvardhan39797@gmail.com>
2025-12-11 21:38:38 +0530

    Done with DeleteBook controller logic

--- BookStore-Management-System/pkg/controllers/book-controller.go
+++ BookStore-Management-System/pkg/controllers/book-controller.go
@@ -43,3 +43,17 @@ func CreateBook(w http.ResponseWriter, r *http.Request){
 	w.WriteHeader(http.StatusOK);
 	w.Write(res);
 }
+
+func DeleteBook(w http.ResponseWriter, r *http.Response){
+	vars := mux.vars(r)
+	bookId := vars["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0,0);
+	if err != nil{
+		fmt.Println("Error while parsing");
+	}
+	book := models.DeleteBook(ID);
+	res,_ := json.Marshal(book);
+	w.Header().Set("Content-Type","pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res);
+}
