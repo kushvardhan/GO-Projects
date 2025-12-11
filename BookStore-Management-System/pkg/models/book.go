3dbe560d1dc8c4ff0e53461dca57f01d3be06af2
Kush Vardhan <kushvardhan39797@gmail.com>
2025-12-11 19:30:26 +0530

    model for Book

--- BookStore-Management-System/pkg/models/book.go
+++ BookStore-Management-System/pkg/models/book.go
@@ -2,8 +2,20 @@ package models
 
 import (
 	"github.com/jinzhu/gorm"
-	"github.com/kushvardhan/GO-Projects/BookStore-Mangement-System/pkg/config"
+	"github.com/kushvardhan/GO-Projects/BookStore-Management-System/pkg/config"
 )
 
-var Db *gorm.DB
+var db *gorm.DB
 
+type Book struct{
+	gorm.model
+	Name string `gorm:""json:"name"`
+	Author string `json:"author"`
+	Publication string `json:"publication"`
+}
+
+func init(){
+	config.ConnectDB()
+	db=config.GetDB()
+	db.AutoMigrate(&Book{})
+}
