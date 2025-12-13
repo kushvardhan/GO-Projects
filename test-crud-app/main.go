edb0499c3792bfd7b772cccf75f0465fb2c22637
Kush Vardhan <kushvardhan39797@gmail.com>
2025-12-13 19:08:22 +0530

    connecting DB from the main file

--- test-crud-app/main.go
+++ test-crud-app/main.go
@@ -1,11 +1,38 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+var mongoClient *mongo.Client;
+
+func init(){
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("env load error", err)
+	}
+
+	log.Println("env file loaded")
+
+	mongoClient, err = mongo.Connect(context.Background(), options.Client().ApplyURI(os.Getenv("MONGO_URL"))
+
+	if err != nil {
+		log.Fatal("Connection error", err)
+	}
+
+	err = mongoClient.Ping(context.Backgound(), readpref.Primary())
+	if err != nil{
+		log.Fatal("ping failed", err)
+	}
+	log.Println("mongo connected");
+}
 
 func main(){
 	r := mux.NewRouter()
